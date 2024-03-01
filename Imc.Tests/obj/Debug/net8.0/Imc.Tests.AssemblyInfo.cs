51e02cd06be0189b6631bb83b7cc792774986b47
evangelista <matheus.evangelista7@protonmail.com>
2024-02-29 22:34:00 -0300

    projeto de teste

--- Imc.Tests/obj/Debug/net8.0/Imc.Tests.AssemblyInfo.cs
+++ Imc.Tests/obj/Debug/net8.0/Imc.Tests.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Imc")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Imc.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Imc")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Imc")]
+[assembly: System.Reflection.AssemblyProductAttribute("Imc.Tests")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Imc.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
